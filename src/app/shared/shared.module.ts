758c103c0e25389ef86a41d03beb4aefd138f9d9
laNICOLEE <nicksva15@gmail.com>
2023-10-25 11:10:46 -0300

    navbar actualizado

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,14 +3,18 @@ import { CommonModule } from '@angular/common';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 
+import { MatIconModule } from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
     FooterComponent,
-    NavbarComponent
+    NavbarComponent,
   ],
   imports: [
     CommonModule,
+    MatIconModule,
+    MatToolbarModule
   ],
   exports: [
   ]
