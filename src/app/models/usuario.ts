71a557131f759b019228450ebf65aa15b1d92216
camila063 <villarroelcamila49@gmail.com>
2023-10-26 11:46:31 -0300

    cambios en register

--- src/app/models/usuario.ts
+++ src/app/models/usuario.ts
@@ -1,7 +1,7 @@
 export interface Usuario {
-    uid: string | any; // id para autentificación de Firebase
-    nombre: string; // -> nombre
-    email: string; // -> email
-    rol: string; // -> roles
+    uid: string | any;
+    nombre: string;
+    email: string;
+    rol: string; 
     contrasena: string;
 }
